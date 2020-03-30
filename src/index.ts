e5d0aa299c0ccacbcb55aeaf5f72ad18e0df5322
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-03-30 15:27:01 +0200

    PORT

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 
 const app = express();
-const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get("/", (req: express.Request, res: express.Response) => {
-    res.send( "Hello world!" );
+    res.send("Hello world!");
 });
 
 // start the Express server
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${ port }`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server started at http://localhost:${process.env.PORT}`);
 });
